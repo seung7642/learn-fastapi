9852082c3c3958d414b16b94614b59bf4c99b4ee
이승호 <seung7642@openbase.co.kr>
2024-07-29 10:24:46 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/hello")
+def hello():
+    return {"message": "안녕하세요. 파이보"}
